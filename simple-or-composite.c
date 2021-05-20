a0b4da00e32cad14de1975e31bc40bcb27984aa8
Maskin-D-A <79995423+Maskin-D-A@users.noreply.github.com>
2021-05-20 16:45:25 +0400

    Add files via upload

---
+++ simple-or-composite.c
@@ -0,0 +1,40 @@
+/*
+Autor: Maskin Danil
+IDE: Embarcadero Dev-C++ v. 6.3
+OS: Windows 10
+*/
+
+#include <stdio.h>
+
+
+
+int main(){
+	int value = 0;
+	int anotherValue = 1;
+	int count = 0;
+	printf("Enter value: ");
+	scanf("%d", &value);
+	if ( value > 1 ){	
+		while ( value >= anotherValue) {
+			if( value % anotherValue == 0){
+					count++; 
+			}
+			anotherValue++;
+		}
+		if(count == 2){
+			printf("This number \"%d\" is simple.\n", value);
+		}
+		else{
+			printf("This number \"%d\" is complicated.\n", value);
+		}	
+	}
+	else {
+		if (value == 1){
+			printf("Number \"1\" is simple. \n");
+		}
+		else {
+			printf("Number\"0\" is simple. \n");
+		}
+	}
+	return 0; 
+}
